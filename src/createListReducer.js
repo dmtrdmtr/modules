8258dfd9d83c93d5157326e8633080ae88ca8914
Ilya Kolesnikov <superbobor@bk.ru>
2017-05-24 17:17:00 +0300

    Replace array-includes to contains (#24)

--- src/createListReducer.js
+++ src/createListReducer.js
@@ -1,6 +1,8 @@
+import contains from 'ramda/src/contains';
+
 export function createListReducer(take, storeBy, reducer) {
     return function(state = {}, action) {
-        if (take.includes(action.type)) {
+        if (contains(action.type, take)) {
             const key = storeBy(action);
             const { [key]: stored = null } = state;
             return { ...state, [key]: reducer(stored, action) };
